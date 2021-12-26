5df875b06e8f0653984ec345c2cea72e2b62ca31
ZDVokoun <gop.2019.2025@gmail.com>
2021-12-26 14:43:18 +0100

    Turn sendNotifications into Promise

--- src/helpers/push-helper.js
+++ src/helpers/push-helper.js
@@ -6,7 +6,7 @@ const vapidDetails = {
   subject: process.env.VAPID_SUBJECT
 };
 
-export async function sendNotifications(title, text, url, subscriptions) {
+export function sendNotifications(title, text, url, subscriptions) {
   const notification = JSON.stringify({
     title,
     body: text,
@@ -16,10 +16,13 @@ export async function sendNotifications(title, text, url, subscriptions) {
     TTL: 10000,
     vapidDetails: vapidDetails
   }
-  await subscriptions.forEach(async subscription => {
+  const newPromise = (subscription) => new Promise((resolve, reject) => {
     const endpoint = subscription.endpoint;
     const id = endpoint.substring(endpoint.length - 8);
-    await webpush.sendNotification(subscription, notification, options)
-      .catch(error => console.error(`Error occured when sending notification to ${id}: ${error}`))
+    return webpush.sendNotification(subscription, notification, options)
+      .then(() => resolve())
+      .catch(error => reject(`Error occured when sending notification to ${id}: ${error}`))
   })
+  return Promise.all(subscriptions.map(item => newPromise(item).catch(err => err)))
+    .then(errs => errs.forEach(err => err && console.error(err)))
 }
